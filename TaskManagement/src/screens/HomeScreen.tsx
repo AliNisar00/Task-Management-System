e453923d64389d97725c9454de92aef3b7d70160
AliNisar00 <syed.alinisar2002@gmail.com>
2024-04-23 12:37:08 +0500

    Add Task FlatList; TODO: add data

--- TaskManagement/src/screens/HomeScreen.tsx
+++ TaskManagement/src/screens/HomeScreen.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { View, Text, StyleSheet, StatusBar, ScrollView, Button } from 'react-native';
 import HeaderBar from '../components/HeaderBar';
+import { FlatList } from 'react-native-gesture-handler';
 
 const HomeScreen = ({ navigation }) => {
     return(
         <View style={styles.ScreenContainer}>
             <StatusBar backgroundColor={'black'} />
             <HeaderBar title='Home' />
+
+            {/* Task Flatlist */}
+            <FlatList showsVerticalScrollIndicator={false} data={}></FlatList>
         </View>
     )
 };
